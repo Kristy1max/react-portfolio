cc37b19b3819fef597270d8d65a7b8e9ab0e32d0
Kristina Zhizherina <diablito.yey@mail.ru>
2021-02-03 23:28:32 +0100

    navbar in process

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,34 @@
-import React, {  } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo">
-            KRISTINA Z.
+            KRISTINA Z. <i className='fas fa-heart' />
           </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu acive' : 'nav-menu'}>
+            <li className="nav-item">
+              <Link to='/home' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to='/projects' className='nav-links' onClick={closeMobileMenu}>
+                Projects
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
