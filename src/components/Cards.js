beba260d6567002aa0352864849dd8b387faa670
Kristina Zhizherina <diablito.yey@mail.ru>
2021-02-07 21:37:24 +0100

    added cards

---
+++ src/components/Cards.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import CardItem from './CardItem';
+import './Cards.scss';
+
+function Cards() {
+  return (
+    <div className='cards'>
+      <h1>My Hobbies</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItem 
+              src='images/luca-bravo-O453M2Liufs-unsplash.jpg'
+              text='Travelling'
+              label='Adventure'
+              path='/projects'
+            />
+            <CardItem 
+              src='images/luca-bravo-O453M2Liufs-unsplash.jpg'
+              text='Travelling'
+              label=''
+              path='/'
+            />
+            <CardItem 
+              src='images/luca-bravo-O453M2Liufs-unsplash.jpg'
+              text='Travelling'
+              label=''
+              path='/'
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../App.scss';
 import HeroSection from '../HeroSection';
+import Cards from '../Cards';
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <Cards />
     </>
   );
 }
