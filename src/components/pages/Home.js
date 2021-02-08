78272cc05803b8ce4bdb245dd7ce4056884869fd
Kristina Zhizherina <diablito.yey@mail.ru>
2021-02-08 23:06:24 +0100

    about section + modifications for other sections done

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import '../../App.scss';
 import HeroSection from '../HeroSection';
-import Cards from '../Cards';
+// import Cards from '../Cards';
 import Footer from '../Footer';
+
+import AboutSection from '../AboutSection';
 
 function Home() {
   return (
     <>
       <HeroSection />
-      <Cards />
+      <AboutSection />
+      {/* <Cards /> */}
       <Footer />
     </>
   );
