61977d5ad58c6a5c3f9f80d39b6f6a124a8fce3f
Kristina Zhizherina <diablito.yey@mail.ru>
2021-02-09 18:51:17 +0100

    skills-section done

--- src/components/SkillsSection.js
+++ src/components/SkillsSection.js
@@ -5,29 +5,54 @@ function SkillsSection() {
   return (
     <section className='skills-section'>
       <h2 className='text-center'><span>Skill set</span></h2>
-
       <div className="skills-container">
         <div className="skills-group">
-          <div className="skills-item hoopsai"><img src="images/luca-bravo-O453M2Liufs-unsplash.jpg"/></div>
+          <figure className="skills-item">
+            <img src="images/icon-vscode.png"/>
+            <figcaption className="caption">Visual Studio</figcaption>
+          </figure>
         </div>
         <div className="skills-group">
-          <div className="skills-item agoda"><img src="images/luca-bravo-O453M2Liufs-unsplash.jpg"/></div>
-          <div className="skills-item getsafe"><img src="images/luca-bravo-O453M2Liufs-unsplash.jpg"/></div>
+          <figure className="skills-item">
+            <img src="images/icon-react.png"/>
+            <figcaption className="caption">Ract</figcaption>
+          </figure>
+          <figure className="skills-item">
+            <img src="images/icon-github.png"/>
+            <figcaption className="caption">Git</figcaption>
+          </figure>
         </div>
         <div className="skills-group">
-          <div className="skills-item grab"><img src="images/luca-bravo-O453M2Liufs-unsplash.jpg"/></div>
-          <div className="skills-item crisp"><img src="images/luca-bravo-O453M2Liufs-unsplash.jpg"/></div>
-          <div className="skills-item anyleads"><img src="images/luca-bravo-O453M2Liufs-unsplash.jpg"/></div>
+          <figure className="skills-item html">
+            <img src="images/icon-html5.png"/>
+            <figcaption className="caption">HTML5</figcaption>
+          </figure>
+          <figure className="skills-item html">
+            <img src="images/icon-css3.png"/>
+            <figcaption className="caption">CSS3</figcaption>
+          </figure>
+          <figure className="skills-item html">
+            <img src="images/icon-js.png"/>
+            <figcaption className="caption">JavaScript</figcaption>
+          </figure>
         </div>
         <div className="skills-group">
-          <div className="skills-item teachable"><img src="images/luca-bravo-O453M2Liufs-unsplash.jpg"/></div>
-          <div className="skills-item flyr"><img src="images/luca-bravo-O453M2Liufs-unsplash.jpg"/></div>
+          <figure className="skills-item">
+            <img src="images/icon-sass.png"/>
+            <figcaption className="caption">Sass</figcaption>
+          </figure>
+          <figure className="skills-item">
+            <img src="images/icon-bootstrap.png"/>
+            <figcaption className="caption">Bootstrap</figcaption>
+          </figure>
         </div>
         <div className="skills-group">
-          <div className="skills-item roger"><img src="images/luca-bravo-O453M2Liufs-unsplash.jpg"/></div>
+          <figure className="skills-item">
+            <img src="images/icon-figma.png"/>
+            <figcaption className="caption">Figma</figcaption>
+          </figure>
         </div>
-      </div>    
-      
+      </div>
     </section>
   )
 }
