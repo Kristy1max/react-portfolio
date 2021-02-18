ec0e4786bf872709440496c70c0f0cd6c0cda01d
Kristina Zhizherina <diablito.yey@mail.ru>
2021-02-18 22:11:42 +0100

    scroll-top-page

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import Projects from './components/pages/Projects';
 
+// Actions
+import ScrollToTop from './components/ScrollToTop';
+
 // Styles
 import './App.scss';
-
 
 function App() {
   return (
     <>
-      <Router>  
+      <Router>
+        <ScrollToTop />
         <Navbar />
           <Switch>
-            <Route path='/home' exact component={Home} />
+            <Route path='/' exact component={Home} />
             <Route path='/projects' component={Projects} />
           </Switch>
       </Router>
