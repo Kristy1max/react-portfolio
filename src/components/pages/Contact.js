a0ad1f2732b0fe897785eb44f20f016d9de8370c
Kristina Zhizherina <diablito.yey@mail.ru>
2021-02-27 18:52:28 +0100

    changes

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import '../Contact.scss';
 
-
 function Contact() {
+
+  const inputs = document.querySelectorAll('.input');
+
+  function focusFunction() {
+    let parent = this.parentNode;
+    parent.classList.add('focus');
+  }
+
+  function blurFunction() {
+    let parent = this.parentNode;
+    if (this.value == '') {
+      parent.classList.remove('focus');
+    }
+  }
+
+  inputs.forEach((input) => {
+    input.addEventListener('focus', focusFunction);
+    input.addEventListener('blur', blurFunction);
+  });
+
   return (
     <div className="container contact">
       <div className="form">
@@ -16,18 +35,27 @@ function Contact() {
             <div className="input-container">
               <input type="text" name='name' className='input'/>
               <label htmlFor="">Name</label>
+              <span>Name</span>
             </div>
             <div className="input-container">
               <input type="email" name='email' className='input'/>
               <label htmlFor="">Email</label>
+              <span>Email</span>
+            </div>
+            <div className="input-container">
+              <input type="tel" name='phone' className='input'/>
+              <label htmlFor="">Phone</label>
+              <span>Phone</span>
             </div>
             <div className="input-container">
               <input type="text" name='name' className='input'/>
               <label htmlFor="">Subject</label>
+              <span>Subject</span>
             </div>
             <div className="input-container textarea">
               <textarea type="text" name='message' className='input'/>
               <label htmlFor="">Message</label>
+              <span>Message</span>
             </div>
             <input type="submit" value="Send" className='btn'/>
           </form>
