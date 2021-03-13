d195dd0002a4653aa2c537d9063baaeb6eaa16ab
Kristina Zhizherina <diablito.yey@mail.ru>
2021-03-13 23:56:34 +0100

    animation 50% done

--- src/components/SkillsSection.js
+++ src/components/SkillsSection.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import './SkillsSection.scss';
+import  sal from 'sal.js';
 
 function SkillsSection() {
+  // ANIMATION LIBRARY
+  sal({
+    threshold: 0.5,
+    once: false,
+    disable: false,
+  });
+
   return (
     <section className='skills-section'>
       <h2 className='text-center'><span>Skill set</span></h2>
       <div className="skills-container">
         <div className="skills-group">
-          <figure className="skills-item">
+          <figure className="skills-item"
+            data-sal-duration="1200"
+            data-sal="slide-right"
+            data-sal-delay="300"
+            data-sal-easing="ease-out-bounce"
+          >
             <img src="images/icon-js.png"/>
             <figcaption className="caption">JavaScript</figcaption>
           </figure>
         </div>
-        <div className="skills-group">
+        <div className="skills-group"
+            data-sal-duration="1200"
+            data-sal="slide-right"
+            data-sal-delay="300"
+            data-sal-easing="ease-out-bounce"
+            >
           <figure className="skills-item">
             <img src="images/icon-react.png"/>
             <figcaption className="caption">React</figcaption>
@@ -23,7 +41,12 @@ function SkillsSection() {
           </figure>
         </div>
         <div className="skills-group">
-          <figure className="skills-item">
+          <figure className="skills-item"
+            data-sal-duration="1200"
+            data-sal="slide-down"
+            data-sal-delay="300"
+            data-sal-easing="ease-out-bounce"
+          >
             <img src="images/icon-html5.png"/>
             <figcaption className="caption">HTML5</figcaption>
           </figure>
@@ -31,12 +54,22 @@ function SkillsSection() {
             <img src="images/icon-sass.png"/>
             <figcaption className="caption">Sass</figcaption>
           </figure>
-          <figure className="skills-item">
+          <figure className="skills-item"
+            data-sal-duration="1200"
+            data-sal="slide-up"
+            data-sal-delay="300"
+            data-sal-easing="ease-out-bounce"
+          >
             <img src="images/icon-vscode.png"/>
             <figcaption className="caption">Visual Studio</figcaption>
           </figure>
         </div>
-        <div className="skills-group">
+        <div className="skills-group"
+          data-sal-duration="1200"
+          data-sal="slide-left"
+          data-sal-delay="300"
+          data-sal-easing="ease-out-bounce"
+        >
           <figure className="skills-item">
             <img src="images/icon-css3.png"/>
             <figcaption className="caption">CSS3</figcaption>
@@ -46,7 +79,12 @@ function SkillsSection() {
             <figcaption className="caption">Figma</figcaption>
           </figure>
         </div>
-        <div className="skills-group">
+        <div className="skills-group"
+          data-sal-duration="1200"
+          data-sal="slide-left"
+          data-sal-delay="300"
+          data-sal-easing="ease-out-bounce"
+        >
           <figure className="skills-item">
             <img src="images/icon-bootstrap.png"/>
             <figcaption className="caption">Bootstrap</figcaption>
